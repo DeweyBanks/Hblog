263e34707369954aec6bc3d5d7d7f97f41f95705
deweybanks <deweybanks@gmail.com>
2019-03-19 15:26:38 -0400

    adds book views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :books
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
